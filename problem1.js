b5b4f20984d68c91a21c4d25e9bb93fdc4080095
Asif Khan <asifkhanajas3@.gmail.com>
2022-12-04 23:25:36 +0530

    Add

---
+++ problem1.js
@@ -0,0 +1,8 @@
+// prime number
+var num=13;
+var count=0;
+for(var i=1;i<=num;i++){
+    if(num%i==0){
+        count++;
+    }
+}
---
+++ problem2.js
@@ -0,0 +1,6 @@
+//Cheak Palindrome
+var str="naman";
+var bag="";
+for(var i=str.length-1;i>=0;i--){
+    bag+=str[i]
+}
